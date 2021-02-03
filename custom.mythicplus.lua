b1c11f1d30f596bb1bf2b29f81c6f7976acde027
pngmn <pngmn@users.noreply.github.com>
2021-02-03 08:44:15 +0100

    Fix mythicplus module not loading

--- custom.mythicplus.lua
+++ custom.mythicplus.lua
@@ -1,16 +1,16 @@
 local addon = KuiNameplates
-local mod = addon:NewPlugin("Custom_MythicAuras", 101)
+local mod = addon:NewPlugin("Custom_MythicPlus", 101)
 if not mod then return end
 local HAS_ENABLED
 
 local function GetBolsterCount(unit)
     local c = 0
-    for i = 1, 40 do
-        local name = UnitAura(unit, i)
-        if name and name == "Bolster" then
+    AuraUtil.ForEachAura(unit, "HELPFUL", nil, function(...)
+        local name = ...
+        if name == "Bolster" then
             c = c + 1
         end
-    end
+    end)
     return c
 end
 
@@ -24,12 +24,12 @@ end
 
 local function PostDisplayAuraButton(f, button)
     if f.id ~= "Custom_MythicBolster" then return end
-    button.count:SetText(GetBolsterCount(f.parent.unit))
+    button.count:SetText(GetBolsterCount(f.parent.unit) > 1 or "")
     button.count:Show()
 end
 
 local function PostUpdateAuraFrame(f)
-    if f.id ~= "Custom_MythicAuras" then return end
+    if f.id ~= "Custom_MythicBolster" then return end
     local c = GetBolsterCount(f.parent.unit)
     if c > 1 then
         for _, button in ipairs(f.buttons) do
@@ -48,25 +48,28 @@ local function EnableAll()
             mod:Create(f)
         else
             f.MythicAuras:Enable()
+            f.MythicBolster:Enable()
         end
     end
     mod:RegisterMessage("Create")
     HAS_ENABLED = true
-    -- print("Custom_MythicAuras enabled.")
+    -- print("Custom_MythicPlus enabled.")
 end
 
 local function DisableAll()
-    for _,f in addon:Frames() do
+    for _, f in addon:Frames() do
         if f.MythicAuras then
             f.MythicAuras:Disable()
+            f.MythicBolster:Disable()
         end
     end
     mod:UnregisterMessage("Create")
-    -- print("Custom_MythicAuras disabled.")
+    -- print("Custom_MythicPlus disabled.")
 end
 
 function mod:Create(f)
-    -- assert(not f.MythicAuras)
+    local size = 28
+    assert(not f.MythicAuras)
     local custom = f.handler:CreateAuraFrame({
         id = "Custom_MythicAuras",
         max = 2,
@@ -79,42 +82,50 @@ function mod:Create(f)
         whitelist = {
             [226510] = true, -- Mythic Plus Affix: Sanguine
             [228318] = true, -- Mythic Plus Affix: Raging
-            [260805] = true, -- Waycrest Manor: Focusing Iris
-            [263246] = true, -- Temple of Sethralis: Lightning Shield
-            [257597] = true, -- MOTHERLODE: Azerite Infusion
+            [343502] = true, -- Mythic Plus Affix: Inspiring
+            -- [260805] = true, -- Waycrest Manor: Focusing Iris
+            -- [263246] = true, -- Temple of Sethralis: Lightning Shield
+            -- [257597] = true, -- MOTHERLODE: Azerite Infusion
         },
     })
     custom:SetFrameLevel(0)
-    custom:SetWidth(42)
-    custom:SetHeight(42)
+    custom:SetWidth(size)
+    custom:SetHeight(size)
+    custom.icon_height = floor(size * .7)
+    custom.icon_ratio = (1 - (custom.icon_height / size)) / 2.5
     custom:SetPoint("BOTTOM", f.bg, "TOP", 0, 42)
     f.MythicAuras = custom
 
+    assert(not f.MythicBolster)
     local bolster = f.handler:CreateAuraFrame({
         id = "Custom_MythicBolster",
         max = 1,
-        size = 24,
+        size = 42,
         squareness = 1,
         timer_threshold = 10,
         pulsate = false,
         point = {"BOTTOMRIGHT", "RIGHT", "LEFT"},
         rows = 1,
         filter = "HELPFUL",
-        whitelist = { [209859] = true -- Mythic Plus Affix: Bolstering
+        whitelist = {
+            [209859] = true -- Mythic Plus Affix: Bolstering
         }
     })
     bolster:SetFrameLevel(0)
-    bolster:SetWidth(24)
-    bolster:SetHeight(24)
-    bolster:SetPoint("BOTTOMLEFT", custom, "BOTTOMRIGHT", 1, 0)
+    bolster:SetWidth(size)
+    bolster:SetHeight(size)
+    bolster.icon_height = floor(size * .7)
+    bolster.icon_ratio = (1 - (bolster.icon_height / size)) / 2.5
+    bolster:SetPoint("BOTTOMLEFT", f.bg, "TOPLEFT", 1, 42)
     f.MythicBolster = bolster
 end
 
 function mod:PLAYER_ENTERING_WORLD()
     if IsInInstance() then
-        local instance_type, difficulty = select(2, GetInstanceInfo())
-        if instance_type == "party" then
-            if difficulty == 8 then
+        local _, instanceType, difficulty = GetInstanceInfo()
+        if instanceType == "party" then
+            local isChallengeMode = select (4, GetDifficultyInfo(difficulty))
+            if isChallengeMode then
                 EnableAll()
                 return
             end
@@ -130,5 +141,5 @@ function mod:Initialise()
     self:RegisterEvent("UPDATE_INSTANCE_INFO", "PLAYER_ENTERING_WORLD")
     self:AddCallback("Auras", "PostCreateAuraButton", PostCreateAuraButton)
     self:AddCallback("Auras", "PostDisplayAuraButton", PostDisplayAuraButton)
-    -- self:AddCallback("Auras", "PostUpdateAuraFrame", PostUpdateAuraFrame)
+    self:AddCallback("Auras", "PostUpdateAuraFrame", PostUpdateAuraFrame)
 end
