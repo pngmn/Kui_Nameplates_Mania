322516d4b186b9f93914c386eaa925d671c90242
pngmn <pngmn@users.noreply.github.com>
2021-05-26 03:01:45 +0200

    Replace spaces with tabs

--- custom.mythicplus.lua
+++ custom.mythicplus.lua
@@ -5,137 +5,137 @@ if not mod then return end
 local HAS_ENABLED
 
 local function GetBolsterCount(unit)
-    local c = 0
-    AuraUtil.ForEachAura(unit, "HELPFUL", nil, function(...)
-        local name = ...
-        if name == "Bolster" then
-            c = c + 1
-        end
-    end)
-    return c
+	local c = 0
+	AuraUtil.ForEachAura(unit, "HELPFUL", nil, function(...)
+		local name = ...
+		if name == "Bolster" then
+			c = c + 1
+		end
+	end)
+	return c
 end
 
 local function PostCreateAuraButton(f, button)
-    if f.id ~= "Custom_MythicBolster" then return end
-    button.count:ClearAllPoints()
-    button.count:SetPoint("Center", button)
-    local font, size, flag = button.cd:GetFont()
-    button.count.fontobject_size = size + 10
+	if f.id ~= "Custom_MythicBolster" then return end
+	button.count:ClearAllPoints()
+	button.count:SetPoint("Center", button)
+	local font, size, flag = button.cd:GetFont()
+	button.count.fontobject_size = size + 10
 end
 
 local function PostDisplayAuraButton(f, button)
-    if f.id ~= "Custom_MythicBolster" then return end
-    button.count:SetText(GetBolsterCount(f.parent.unit) > 1 or "")
-    button.count:Show()
+	if f.id ~= "Custom_MythicBolster" then return end
+	button.count:SetText(GetBolsterCount(f.parent.unit) > 1 or "")
+	button.count:Show()
 end
 
 local function PostUpdateAuraFrame(f)
-    if f.id ~= "Custom_MythicBolster" then return end
-    local c = GetBolsterCount(f.parent.unit)
-    if c > 1 then
-        for _, button in ipairs(f.buttons) do
-            if button.spellid == 209859 then
-                button.count:SetText(c)
-                button.count:Show()
-                break
-            end
-        end
-    end
+	if f.id ~= "Custom_MythicBolster" then return end
+	local c = GetBolsterCount(f.parent.unit)
+	if c > 1 then
+		for _, button in ipairs(f.buttons) do
+			if button.spellid == 209859 then
+				button.count:SetText(c)
+				button.count:Show()
+				break
+			end
+		end
+	end
 end
 
 local function EnableAll()
-    for _, f in addon:Frames() do
-        if not f.MythicAuras then
-            mod:Create(f)
-        else
-            f.MythicAuras:Enable()
-            f.MythicBolster:Enable()
-        end
-    end
-    mod:RegisterMessage("Create")
-    HAS_ENABLED = true
-    -- print("Custom_MythicPlus enabled.")
+	for _, f in addon:Frames() do
+		if not f.MythicAuras then
+			mod:Create(f)
+		else
+			f.MythicAuras:Enable()
+			f.MythicBolster:Enable()
+		end
+	end
+	mod:RegisterMessage("Create")
+	HAS_ENABLED = true
+	-- print("Custom_MythicPlus enabled.")
 end
 
 local function DisableAll()
-    for _, f in addon:Frames() do
-        if f.MythicAuras then
-            f.MythicAuras:Disable()
-            f.MythicBolster:Disable()
-        end
-    end
-    mod:UnregisterMessage("Create")
-    -- print("Custom_MythicPlus disabled.")
+	for _, f in addon:Frames() do
+		if f.MythicAuras then
+			f.MythicAuras:Disable()
+			f.MythicBolster:Disable()
+		end
+	end
+	mod:UnregisterMessage("Create")
+	-- print("Custom_MythicPlus disabled.")
 end
 
 function mod:Create(f)
-    local size = 28
-    assert(not f.MythicAuras)
-    local custom = f.handler:CreateAuraFrame({
-        id = "Custom_MythicAuras",
-        max = 2,
-        size = core:Scale(core.profile.auras_icon_normal_size),
-        squareness = core.profile.auras_icon_squareness,
-        point = {"BOTTOMLEFT", "LEFT", "RIGHT"},
-        rows = 1,
-        filter = "HELPFUL",
-        whitelist = {
-            [226510] = true, -- Mythic Plus Affix: Sanguine
-            [228318] = true, -- Mythic Plus Affix: Raging
-            [343502] = true, -- Mythic Plus Affix: Inspiring
-        },
-    })
-    custom:SetFrameLevel(0)
-    custom:SetWidth(size)
-    custom:SetHeight(size)
-    -- custom.icon_height = floor(size * .7)
-    -- custom.icon_ratio = (1 - (custom.icon_height / size)) / 2.5
-    custom:SetPoint("BOTTOMLEFT", f.bg, "TOPLEFT", 0, 42)
-    f.MythicAuras = custom
+	local size = 28
+	assert(not f.MythicAuras)
+	local custom = f.handler:CreateAuraFrame({
+		id = "Custom_MythicAuras",
+		max = 2,
+		size = core:Scale(core.profile.auras_icon_normal_size),
+		squareness = core.profile.auras_icon_squareness,
+		point = {"BOTTOMLEFT", "LEFT", "RIGHT"},
+		rows = 1,
+		filter = "HELPFUL",
+		whitelist = {
+			[226510] = true, -- Mythic Plus Affix: Sanguine
+			[228318] = true, -- Mythic Plus Affix: Raging
+			[343502] = true, -- Mythic Plus Affix: Inspiring
+		},
+	})
+	custom:SetFrameLevel(0)
+	custom:SetWidth(size)
+	custom:SetHeight(size)
+	-- custom.icon_height = floor(size * .7)
+	-- custom.icon_ratio = (1 - (custom.icon_height / size)) / 2.5
+	custom:SetPoint("BOTTOMLEFT", f.bg, "TOPLEFT", 0, 42)
+	f.MythicAuras = custom
 
-    assert(not f.MythicBolster)
-    local bolster = f.handler:CreateAuraFrame({
-        id = "Custom_MythicBolster",
-        max = 1,
-        size = core:Scale(core.profile.auras_icon_normal_size),
-        squareness = core.profile.auras_icon_squareness,
-        point = {"BOTTOMRIGHT", "RIGHT", "LEFT"},
-        rows = 1,
-        filter = "HELPFUL",
-        whitelist = {
-            [209859] = true -- Mythic Plus Affix: Bolstering
-        }
-    })
-    bolster:SetFrameLevel(0)
-    bolster:SetWidth(size)
-    bolster:SetHeight(size)
-    -- bolster.icon_height = floor(size * .7)
-    -- bolster.icon_ratio = (1 - (bolster.icon_height / size)) / 2.5
-    bolster:SetPoint("BOTTOMRIGHT", f.bg, "TOPRIGHT", 0, 42)
-    f.MythicBolster = bolster
+	assert(not f.MythicBolster)
+	local bolster = f.handler:CreateAuraFrame({
+		id = "Custom_MythicBolster",
+		max = 1,
+		size = core:Scale(core.profile.auras_icon_normal_size),
+		squareness = core.profile.auras_icon_squareness,
+		point = {"BOTTOMRIGHT", "RIGHT", "LEFT"},
+		rows = 1,
+		filter = "HELPFUL",
+		whitelist = {
+			[209859] = true -- Mythic Plus Affix: Bolstering
+		}
+	})
+	bolster:SetFrameLevel(0)
+	bolster:SetWidth(size)
+	bolster:SetHeight(size)
+	-- bolster.icon_height = floor(size * .7)
+	-- bolster.icon_ratio = (1 - (bolster.icon_height / size)) / 2.5
+	bolster:SetPoint("BOTTOMRIGHT", f.bg, "TOPRIGHT", 0, 42)
+	f.MythicBolster = bolster
 end
 
 function mod:PLAYER_ENTERING_WORLD()
-    if IsInInstance() then
-        local _, instanceType, difficulty = GetInstanceInfo()
-        if instanceType == "party" then
-            local name, groupType, isHeroic, isChallengeMode, displayHeroic, displayMythic, toggleDifficultyID = GetDifficultyInfo(difficulty)
-            if isChallengeMode then
-                EnableAll()
-                return
-            end
-        end
-    end
-    if HAS_ENABLED then
-        DisableAll()
-    end
+	if IsInInstance() then
+		local _, instanceType, difficulty = GetInstanceInfo()
+		if instanceType == "party" then
+			local name, groupType, isHeroic, isChallengeMode, displayHeroic, displayMythic, toggleDifficultyID = GetDifficultyInfo(difficulty)
+			if isChallengeMode then
+				EnableAll()
+				return
+			end
+		end
+	end
+	if HAS_ENABLED then
+		DisableAll()
+	end
 end
 
 function mod:OnEnable()
-    self:RegisterEvent("PLAYER_ENTERING_WORLD")
-    self:RegisterEvent("UPDATE_INSTANCE_INFO", "PLAYER_ENTERING_WORLD")
-    self:RegisterEvent("CHALLENGE_MODE_START", "PLAYER_ENTERING_WORLD")
-    self:AddCallback("Auras", "PostCreateAuraButton", PostCreateAuraButton)
-    self:AddCallback("Auras", "PostDisplayAuraButton", PostDisplayAuraButton)
-    self:AddCallback("Auras", "PostUpdateAuraFrame", PostUpdateAuraFrame)
+	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+	self:RegisterEvent("UPDATE_INSTANCE_INFO", "PLAYER_ENTERING_WORLD")
+	self:RegisterEvent("CHALLENGE_MODE_START", "PLAYER_ENTERING_WORLD")
+	self:AddCallback("Auras", "PostCreateAuraButton", PostCreateAuraButton)
+	self:AddCallback("Auras", "PostDisplayAuraButton", PostDisplayAuraButton)
+	self:AddCallback("Auras", "PostUpdateAuraFrame", PostUpdateAuraFrame)
 end
