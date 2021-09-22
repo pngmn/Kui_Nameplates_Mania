4efd36bdb1df6e03ef2a9401d53b759778d11981
pngmn <pngmn@users.noreply.github.com>
2021-09-22 19:48:38 +0200

    Small performance fix

--- custom.name-short.lua
+++ custom.name-short.lua
@@ -3,7 +3,7 @@ local mod = addon:NewPlugin("Custom_NameShort", 101)
 if not mod then return end
 
 function mod:Show(plate)
-    plate.NameText:SetText(plate.state.name:gsub('(%S+) ', function(t) return t:sub(1,1)..'. ' end))
+    plate.NameText:SetText(gsub(plate.state.name, "(%S+) ", function(t) return t:sub(1, 1) .. ". " end))
 end
 
 function mod:Initialise()
